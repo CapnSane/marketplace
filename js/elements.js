a368a2b57ffb521427b3cff84c40cb2678b32a92
Leonardo Schneider <lnrdschneider@gmail.com>
2021-02-15 20:55:15 -0300

    feat: store button with hide & show toggle function

--- js/elements.js
+++ js/elements.js
@@ -1,6 +1,11 @@
-// function jorje() {
-//   document.getElementById("box").classList.toggle("hide");
-// }
+function hideShow() {
+  var x = document.getElementById("cardDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -12,17 +17,17 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
